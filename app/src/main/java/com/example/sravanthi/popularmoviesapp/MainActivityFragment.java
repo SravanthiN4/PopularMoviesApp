dc4145218b1d78b92dd27afdd6ba716349053464
SravanthiN4 <snedunoori44@gmail.com>
2016-04-25 15:29:41 -0400

    removed dynamic fragment and added built uri

--- app/src/main/java/com/example/sravanthi/popularmoviesapp/MainActivityFragment.java
+++ app/src/main/java/com/example/sravanthi/popularmoviesapp/MainActivityFragment.java
@@ -16,6 +16,10 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -72,12 +76,37 @@ public class MainActivityFragment extends Fragment {
         return rootView;
     }
 
-    public class FetchPosterTask extends AsyncTask<Void,Void,Void>
+    public class FetchPosterTask extends AsyncTask<String, Void, String>
     {
         private final String LOG_TAG = FetchPosterTask.class.getSimpleName();
 
+        private String getPosterFromJson(String posterJsonStr) throws JSONException
+        {
+            final String MDB_RESULTS = "results";
+            final String MDB_POSTER_PATH = "poster_path";
+            String resultStrs = null;
+            JSONObject posterJson = new JSONObject(posterJsonStr);
+            JSONArray movieArray = posterJson.getJSONArray(MDB_RESULTS);
+            for(int i=0; i<movieArray.length();i++)
+            {
+                JSONObject posterPathObject = movieArray.getJSONObject(i);
+                String name = posterPathObject.getString(MDB_POSTER_PATH).toString();
+                resultStrs = "posters:" +name;
+            }
+
+            Log.v(LOG_TAG,"Posters:"+resultStrs);
+            return resultStrs;
+
+
+        }
+
+
+
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             //will contain the json response as a String
@@ -88,56 +117,58 @@ public class MainActivityFragment extends Fragment {
                 final String APIKEY_PARAM = "api_key";
 
                 Uri builtUri = Uri.parse(POSTER_BASE_URL).buildUpon()
-                        .appendQueryParameter(APIKEY_PARAM,BuildConfig.THE_MOVIE_DB_API_KEY)
+                        .appendQueryParameter(APIKEY_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY)
                         .build();
 
                 URL url = new URL(builtUri.toString());
 
-                Log.v(LOG_TAG,"Built URI "+builtUri.toString());
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
 
-                urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuffer buffer = new StringBuffer();
-                if(inputStream == null)
-                {
+                if (inputStream == null) {
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
                 String line;
-                while ((line = reader.readLine())!=null)
-                {
-                    buffer.append(line+"\n");
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
                 }
-                if(buffer.length()==0)
-                {
+                if (buffer.length() == 0) {
                     return null;
                 }
                 posterJsonStr = buffer.toString();
-                Log.v(LOG_TAG, "Poster Json String:"+posterJsonStr);
-            }catch (IOException e)
-            {
-                Log.e(LOG_TAG, "Error",e);
+                Log.v(LOG_TAG, "Poster Json String:" + posterJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
                 return null;
-            }
-            finally {
-                if(urlConnection!=null)
-                {
+            } finally {
+                if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
-                if(reader!=null)
-                {
+                if (reader != null) {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e(LOG_TAG,"Error closing stream",e);
+                        Log.e(LOG_TAG, "Error closing stream", e);
                     }
                 }
             }
+
+            try {
+                return getPosterFromJson(posterJsonStr);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             return null;
         }
+
+
     }
 }
